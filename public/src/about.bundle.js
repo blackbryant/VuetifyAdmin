1f88d9d45476c6d2fb16e21cc43bd06bfeda79ea
blackbryant <e49134030e@hotmail>
2021-12-11 09:04:28 +0800

    good

---
+++ public/src/about.bundle.js
@@ -0,0 +1,62 @@
+"use strict";
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkvuetify_demo"] = self["webpackChunkvuetify_demo"] || []).push([["about"],{
+
+/***/ "./src/views/home.vue":
+/*!****************************!*\
+  !*** ./src/views/home.vue ***!
+  \****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _home_vue_vue_type_template_id_7eb2bc79___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./home.vue?vue&type=template&id=7eb2bc79& */ \"./src/views/home.vue?vue&type=template&id=7eb2bc79&\");\n/* harmony import */ var _home_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./home.vue?vue&type=script&lang=js& */ \"./src/views/home.vue?vue&type=script&lang=js&\");\n/* harmony import */ var _node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! !../../node_modules/vue-loader/lib/runtime/componentNormalizer.js */ \"./node_modules/vue-loader/lib/runtime/componentNormalizer.js\");\n\n\n\n\n\n/* normalize component */\n;\nvar component = (0,_node_modules_vue_loader_lib_runtime_componentNormalizer_js__WEBPACK_IMPORTED_MODULE_2__[\"default\"])(\n  _home_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_1__[\"default\"],\n  _home_vue_vue_type_template_id_7eb2bc79___WEBPACK_IMPORTED_MODULE_0__.render,\n  _home_vue_vue_type_template_id_7eb2bc79___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns,\n  false,\n  null,\n  null,\n  null\n  \n)\n\n/* hot reload */\nif (false) { var api; }\ncomponent.options.__file = \"src/views/home.vue\"\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (component.exports);\n\n//# sourceURL=webpack://vuetify_demo/./src/views/home.vue?");
+
+/***/ }),
+
+/***/ "./node_modules/vue-loader/lib/index.js??vue-loader-options!./src/views/home.vue?vue&type=script&lang=js&":
+/*!****************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/index.js??vue-loader-options!./src/views/home.vue?vue&type=script&lang=js& ***!
+  \****************************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n//\n\n  \n//import HelloWorld from '../components/HelloWorld'\n//import Toolbars from '../components/Toolbars'\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = ({\n  name: 'Home',\n  components: {\n  //  Toolbars, \n  //  HelloWorld\n     \n  }\n});\n\n\n//# sourceURL=webpack://vuetify_demo/./src/views/home.vue?./node_modules/vue-loader/lib/index.js??vue-loader-options");
+
+/***/ }),
+
+/***/ "./src/views/home.vue?vue&type=script&lang=js&":
+/*!*****************************************************!*\
+  !*** ./src/views/home.vue?vue&type=script&lang=js& ***!
+  \*****************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/* harmony import */ var _node_modules_vue_loader_lib_index_js_vue_loader_options_home_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../node_modules/vue-loader/lib/index.js??vue-loader-options!./home.vue?vue&type=script&lang=js& */ \"./node_modules/vue-loader/lib/index.js??vue-loader-options!./src/views/home.vue?vue&type=script&lang=js&\");\n /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (_node_modules_vue_loader_lib_index_js_vue_loader_options_home_vue_vue_type_script_lang_js___WEBPACK_IMPORTED_MODULE_0__[\"default\"]); \n\n//# sourceURL=webpack://vuetify_demo/./src/views/home.vue?");
+
+/***/ }),
+
+/***/ "./src/views/home.vue?vue&type=template&id=7eb2bc79&":
+/*!***********************************************************!*\
+  !*** ./src/views/home.vue?vue&type=template&id=7eb2bc79& ***!
+  \***********************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"render\": () => (/* reexport safe */ _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_home_vue_vue_type_template_id_7eb2bc79___WEBPACK_IMPORTED_MODULE_0__.render),\n/* harmony export */   \"staticRenderFns\": () => (/* reexport safe */ _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_home_vue_vue_type_template_id_7eb2bc79___WEBPACK_IMPORTED_MODULE_0__.staticRenderFns)\n/* harmony export */ });\n/* harmony import */ var _node_modules_vue_loader_lib_loaders_templateLoader_js_vue_loader_options_node_modules_vue_loader_lib_index_js_vue_loader_options_home_vue_vue_type_template_id_7eb2bc79___WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! -!../../node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!../../node_modules/vue-loader/lib/index.js??vue-loader-options!./home.vue?vue&type=template&id=7eb2bc79& */ \"./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./src/views/home.vue?vue&type=template&id=7eb2bc79&\");\n\n\n//# sourceURL=webpack://vuetify_demo/./src/views/home.vue?");
+
+/***/ }),
+
+/***/ "./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./src/views/home.vue?vue&type=template&id=7eb2bc79&":
+/*!**************************************************************************************************************************************************************************************************!*\
+  !*** ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options!./src/views/home.vue?vue&type=template&id=7eb2bc79& ***!
+  \**************************************************************************************************************************************************************************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"render\": () => (/* binding */ render),\n/* harmony export */   \"staticRenderFns\": () => (/* binding */ staticRenderFns)\n/* harmony export */ });\nvar render = function () {\n  var _vm = this\n  var _h = _vm.$createElement\n  var _c = _vm._self._c || _h\n  return _c(\n    \"div\",\n    { staticClass: \"home\" },\n    [\n      _c(\"h1\", [_vm._v(\"HomePage\")]),\n      _vm._v(\" \"),\n      _c(\"p\", { staticClass: \"red lighten-4 blue--text text--darken-4 \" }, [\n        _vm._v(\"dsdf  \"),\n      ]),\n      _vm._v(\" \"),\n      _c(\"p\", { staticClass: \"red lighten-4 blue--text text--lighten-1 \" }, [\n        _vm._v(\"dsdf  \"),\n      ]),\n      _vm._v(\" \"),\n      _c(\"v-btn\", { staticClass: \"hidden-md-and-down\" }, [\n        _vm._v(\" Click Me Down\"),\n      ]),\n      _vm._v(\" \"),\n      _c(\"v-btn\", { staticClass: \"hidden-md-and-up\" }, [\n        _vm._v(\" Click Me Up\"),\n      ]),\n      _vm._v(\" \"),\n      _c(\"v-btn\", { staticClass: \"pink white--text\" }, [\n        _vm._v(\" click Me (white--text)\"),\n      ]),\n      _vm._v(\" \"),\n      _c(\"v-btn\", { staticClass: \"pink\", attrs: { depressed: \"\" } }, [\n        _vm._v(\" click Me (depressed)\"),\n      ]),\n      _vm._v(\" \"),\n      _c(\"v-btn\", { staticClass: \"pink\", attrs: { flat: \"\" } }, [\n        _vm._v(\" click Me (flat)\"),\n      ]),\n      _vm._v(\" \"),\n      _c(\n        \"v-btn\",\n        { staticClass: \"pink white--text\", attrs: { depressed: \"\" } },\n        [\n          _c(\"v-icon\", [_vm._v(\"email\")]),\n          _vm._v(\" \"),\n          _c(\"span\", [_vm._v(\" email Me\")]),\n        ],\n        1\n      ),\n      _vm._v(\" \"),\n      _c(\n        \"v-btn\",\n        {\n          staticClass: \"pink white--text\",\n          attrs: { small: \"\", depressed: \"\" },\n        },\n        [\n          _c(\"v-icon\", [_vm._v(\"email\")]),\n          _vm._v(\" \"),\n          _c(\"span\", [_vm._v(\" email Me\")]),\n        ],\n        1\n      ),\n      _vm._v(\" \"),\n      _c(\n        \"v-btn\",\n        {\n          staticClass: \"pink white--text\",\n          attrs: { large: \"\", depressed: \"\" },\n        },\n        [\n          _c(\"span\", [_vm._v(\" email Me\")]),\n          _vm._v(\" \"),\n          _c(\"v-icon\", { attrs: { right: \"\" } }, [_vm._v(\"email\")]),\n        ],\n        1\n      ),\n      _vm._v(\" \"),\n      _c(\n        \"v-btn\",\n        { staticClass: \"ma-2\", attrs: { color: \"primary\" } },\n        [\n          _vm._v(\"\\n      Accept\\n      \"),\n          _c(\"v-icon\", { attrs: { right: \"\" } }, [\n            _vm._v(\"\\n        marked-circle\\n      \"),\n          ]),\n        ],\n        1\n      ),\n    ],\n    1\n  )\n}\nvar staticRenderFns = []\nrender._withStripped = true\n\n\n\n//# sourceURL=webpack://vuetify_demo/./src/views/home.vue?./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib/index.js??vue-loader-options");
+
+/***/ })
+
+}]);
